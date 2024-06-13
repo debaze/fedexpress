1968be9f9f78fa954916904f4e33a6a54642ef20
florian f <furnari.florian@hotmail.fr>
2024-06-13 17:17:53 +0200

    fix : review PR

--- controllers/barController.js
+++ controllers/barController.js
@@ -6,20 +6,23 @@ const barController = {
 			const result = await Bar.findAll()
 			return res.status(200).send(result)
 		} catch (error) {
-			return res.status(500).send({ message: 'Error retrieve bar: ', error })
+			return res.status(500).send({ message: 'Error read bar: ', error })
 		}
 	},
 
 	async read(req, res) {
 		const id = req.params.barId
-		const result = await Bar.findByPk(id)
 		try {
+			const result = await Bar.findByPk(id)
+			if (!result) {
+				return res.status(404).send({ message: 'Bar missing' });
+			}
 			return res.status(200).send(result)
 		} catch (error) {
-			return res.status(500).send({ message: 'Error retrieve bar: ', error })
+			return res.status(500).send({ message: 'Error read bar: ', error })
 		}
 	},
-	
+
 	async create(req, res) {
 			const bar = { name, address, tel, email, description } = req.body
 			try {
@@ -34,7 +37,10 @@ const barController = {
 			const id = req.params.barId
 			const bar = { name, address, tel, email, description } = req.body
 			try {
-				const result = await Bar.update(bar, { where: { id: id } })
+				const result = await Bar.update(bar, { where: { id } })
+				if (!result) {
+					return res.status(404).send({ message: 'Bar missing' });
+				}
 				return res.status(200).send(result)
 			} catch (error) {
 				return res.status(500).send({ message: 'Error update bar: ', error })
@@ -44,14 +50,14 @@ const barController = {
 	async delete (req, res) {
 			const id = req.params.barId
 			try {
-				const result = await Bar.destroy({ where: { id: id } })
-				if (result === 0) {
+				const result = await Bar.destroy({ where: { id } })
+				if (!result) {
 					return res.status(404).send({ message: 'Bar missing' });
 				}
 				return res.status(200).send({ message: 'Bar delete with success' });
 
 			} catch (error) {
-				return res.status(500).send({ message: 'Error update bar: ', error })
+				return res.status(500).send({ message: 'Error delete bar: ', error })
 			}
 		},
 	};
