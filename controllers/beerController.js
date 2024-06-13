67e0fdea09ca895a6d1454a6238b31c3462124bc
Damien Mouchagues <damienmouchagues33@gmail.com>
2024-06-13 17:30:03 +0200

    fix: review changes PR

--- controllers/beerController.js
+++ controllers/beerController.js
@@ -9,25 +9,27 @@ const beerController = {
     const id = req.params.beerId;
     const b = await Beer.findByPk(id);
     if (!b) {
-      res.status(404).send({ message: "Beer not found" });
+      return res.status(404).send({ message: "Beer not found" });
     }
     return res.status(200).send(b);
   },
   async create(req, res) {
     const barId = req.params.barId;
     const { name, degree, price, description } = req.body;
-    const beer = { name, degree, price, description, barId };
+    const beer = { name, degree, price, description, BarId: barId };
 
     const b = await Beer.create(beer);
     return res.status(201).send({ beer: b, message: "Beer created" });
   },
   async update(req, res) {
     const id = req.params.beerId;
-    const { name, degree, price, description } = req.body;
-    const beer = { name, degree, price, description };
+    const { name, degree, price, description, barId } = req.body;
+    const beer = { name, degree, price, description, BarId: barId };
 
     const queryResult = await Beer.update(beer, { where: { id } });
-    if (!queryResult) return res.status(404).send("Beer not found");
+    if (!queryResult) {
+      return res.status(404).send("Beer not found");
+    }
     return res
       .status(200)
       .send({ message: "Beer updated", result: queryResult });
@@ -35,7 +37,9 @@ const beerController = {
   async delete(req, res) {
     const id = req.params.beerId;
     const queryResult = await Beer.destroy({ where: { id } });
-    if (!queryResult) return res.status(404).send("Beer not found");
+    if (!queryResult) {
+      return res.status(404).send("Beer not found");
+    }
     return res
       .status(200)
       .send({ message: "Beer deleted", result: queryResult });
