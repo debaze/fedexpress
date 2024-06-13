198b51b5fadff9b6819e8f606adeda60069c5b57
matteokeole <matteo@keole.net>
2024-06-13 16:01:43 +0200

    feat: Read one and create order API routes

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -5,11 +5,32 @@ const orderController = {
 	async readAll() {
 		//
 	},
-	async read() {
-		//
+	async readOne(request, response) {
+		const orderId = request.params.orderId;
+		const order = await Order.findByPk(orderId);
+
+		if (!order) {
+			return response
+				.status(404)
+				.json("No order found with the given ID.");
+		}
+
+		return response
+			.status(200)
+			.json(order);
 	},
-	async create() {
-		//
+	async create(request, response) {
+		if (!request.form.isValid) {
+			return response
+				.status(400)
+				.json("Invalid form.");
+		}
+
+		const order = await Order.create(request.form);
+
+		return response
+			.status(201)
+			.json(order);
 	},
 	async update() {
 		//
--- routers/orderRouter.js
+++ routers/orderRouter.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const orderController = require("../controllers/orderController");
+const createOrderForm = require("../forms/order/createOrderForm");
 
 const router = express.Router();
 
 router.get("/bars/:barId/orders", orderController.readAll);
-router.get("/orders/:orderId", orderController.read);
-router.post("/bars/:barId/orders", orderController.create);
+router.get("/orders/:orderId", orderController.readOne);
+router.post("/bars/:barId/orders", createOrderForm, orderController.create);
 router.patch("/orders/:orderId", orderController.update);
 router.delete("/orders/:orderId", orderController.delete);
 
