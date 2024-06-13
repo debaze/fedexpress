610c8efe0f9fc32b07ff24a999f22c4337b5e0f6
florian f <furnari.florian@hotmail.fr>
2024-06-13 16:13:16 +0200

    feat: add route bar

--- controllers/barController.js
+++ controllers/barController.js
@@ -1,20 +1,52 @@
 const Bar = require("../models/Bar");
 
 const barController = {
-	async readAll() {
-		//
+	async readAll(req, res) {
+		try {
+			const result = await Bar.findAll()
+			return res.status(200).send(result)
+		} catch (error) {
+			return res.status(500).send({ message: 'Error retrieve bar: ', error })
+		}
 	},
-	async read() {
-		//
+
+	async read(req, res) {
+		const id = req.params.barId
+			const result = await Bar.findByPk(id)
+			if(result != null){
+				return res.status(200).send(result)
+			}
 	},
-	async create() {
-		//
+
+	async create(req, res) {
+		const bar = { name, address, tel, email, description } = req.body
+		try {
+			const result = await Bar.create(bar)
+			return res.status(200).send(result)
+		} catch (error) {
+			return res.status(500).send({ message: 'Error create bar: ', error })
+		}
 	},
-	async update() {
-		//
+
+	async update(req, res) {
+		const id = req.params.barId
+		const bar = { name, address, tel, email, description } = req.body
+		try {
+			const result = await Bar.update(bar, { where : {id : id}})
+			return res.status(200).send(result)
+		} catch (error) {
+			return res.status(500).send({ message: 'Error update bar: ', error })
+		}
+
 	},
 	async delete() {
-		//
+		const id = req.params.barId
+		try {
+			const result = await Bar.destroy({ where : {id : id}})
+			return res.status(200).send(result)
+		} catch (error) {
+			return res.status(500).send({ message: 'Error update bar: ', error })
+		}
 	},
 };
 
--- routers/beerOrderRouter.js
+++ routers/beerOrderRouter.js
@@ -1,5 +1,5 @@
-const express = require("express");
 const beerOrderController = require("../controllers/beerOrderController");
+const express = require("express");
 
 const router = express.Router();
 
