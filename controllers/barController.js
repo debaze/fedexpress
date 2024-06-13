e8702aede262e1781525e541f233f54041dc82bb
florian f <furnari.florian@hotmail.fr>
2024-06-13 16:32:31 +0200

    fix : fix route delete and update

--- controllers/barController.js
+++ controllers/barController.js
@@ -12,42 +12,48 @@ const barController = {
 
 	async read(req, res) {
 		const id = req.params.barId
-			const result = await Bar.findByPk(id)
-			if(result != null){
-				return res.status(200).send(result)
-			}
-	},
-
-	async create(req, res) {
-		const bar = { name, address, tel, email, description } = req.body
+		const result = await Bar.findByPk(id)
 		try {
-			const result = await Bar.create(bar)
 			return res.status(200).send(result)
 		} catch (error) {
-			return res.status(500).send({ message: 'Error create bar: ', error })
+			return res.status(500).send({ message: 'Error retrieve bar: ', error })
 		}
 	},
+	
+	async create(req, res) {
+			const bar = { name, address, tel, email, description } = req.body
+			try {
+				const result = await Bar.create(bar)
+				return res.status(200).send(result)
+			} catch (error) {
+				return res.status(500).send({ message: 'Error create bar: ', error })
+			}
+		},
 
 	async update(req, res) {
-		const id = req.params.barId
-		const bar = { name, address, tel, email, description } = req.body
-		try {
-			const result = await Bar.update(bar, { where : {id : id}})
-			return res.status(200).send(result)
-		} catch (error) {
-			return res.status(500).send({ message: 'Error update bar: ', error })
-		}
+			const id = req.params.barId
+			const bar = { name, address, tel, email, description } = req.body
+			try {
+				const result = await Bar.update(bar, { where: { id: id } })
+				return res.status(200).send(result)
+			} catch (error) {
+				return res.status(500).send({ message: 'Error update bar: ', error })
+			}
 
-	},
-	async delete() {
-		const id = req.params.barId
-		try {
-			const result = await Bar.destroy({ where : {id : id}})
-			return res.status(200).send(result)
-		} catch (error) {
-			return res.status(500).send({ message: 'Error update bar: ', error })
-		}
-	},
-};
+		},
+	async delete (req, res) {
+			const id = req.params.barId
+			try {
+				const result = await Bar.destroy({ where: { id: id } })
+				if (result === 0) {
+					return res.status(404).send({ message: 'Bar missing' });
+				}
+				return res.status(200).send({ message: 'Bar delete with success' });
 
-module.exports = barController;
+			} catch (error) {
+				return res.status(500).send({ message: 'Error update bar: ', error })
+			}
+		},
+	};
+
+	module.exports = barController;
