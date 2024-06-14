6ea73bf1675e3bc85a2e1192b48ac0ad203564a1
matteokeole <matteo@keole.net>
2024-06-14 12:34:23 +0200

    feat: Bar average degree

--- routers/barRouter.js
+++ routers/barRouter.js
@@ -7,6 +7,7 @@ const router = express.Router();
 
 router.get("/", barController.readAll);
 router.get("/:barId", barController.read);
+router.get("/:barId/degree", barController.readAverageDegree);
 router.post("/", createBarForm ,barController.create);
 router.patch("/:barId", updateBarForm,barController.update);
 router.delete("/:barId", barController.delete);
